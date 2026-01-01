af1a5cd7033cdbea1882cb9744851d86e097efb2
mohamedelsadany46 <mohamedsadany04@gmail.com>
2026-01-02 01:02:08 +0200

    html and css

---
+++ scripts/budget-calculator.js
@@ -0,0 +1,7 @@
+/*
+  ID & Name:        Youssef Atta Mohamed - 223103574, Mohamed Ahmed Mohamed - 224200083, Wahballah ahmed wahballah - 221100971, Ziad Abdullah - 221101546
+  Course:           CSE211 Web Programming
+  Assignment:       Course Project
+  Date:             30/12/2025
+  Description:      Budget calculator functionality for event cost calculation
+*/
